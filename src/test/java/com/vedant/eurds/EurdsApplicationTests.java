c6cb00af20265a6a93179558964f683d6194ec22
Gamerboyi <itsvedantnautiyal@gmail.com>
2026-03-03 19:49:54 +0530

    Phase 0: Project setup, database schema, docker config

---
+++ src/test/java/com/vedant/eurds/EurdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedant.eurds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
